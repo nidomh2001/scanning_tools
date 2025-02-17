117fd6362d7cfd8d79a11d28044d536982ed3c6d
nidomh2001 <niidome.akitoshi.n7@s.mail.nagoya-u.ac.jp>
2025-02-17 11:43:09 +0900

    [update] 分解能やBASE_LATE等の内容を考慮してstagecontroller_xx.pyの内容を変更。デバッグ済み

--- module/stagecontroller_shot304gs.py
+++ module/stagecontroller_shot304gs.py
@@ -4,6 +4,15 @@ import time
 
 READY_CHECK_PERIOD_S = 1/1000
 
+# TODO: BASE_LATEの値は使用するステージのネジリード[mm] × 20
+# 例1: ネジリード1mmのSGSP20-35の場合 ：BASE_LATE = 1 × 20 = 20
+# 例2: ネジリード2mmのSGSP26-150の場合：BASE_LATE = 2 × 20 = 40
+BASE_LATE1 = 40 # 軸1のBASE_LATE
+BASE_LATE2 = 40 # 軸2のBASE_LATE
+BASE_LATE3 = 40 # 軸1のBASE_LATE
+BASE_LATE4 = 40 # 軸2のBASE_LATE
+# TODO: ステージコントローラのメモリスイッチの値も同様に設定してください
+
 class StageController:
     
     def __init__(self):
@@ -75,6 +84,7 @@ class StageController:
     # axis3_resolution: Axis 3の分解能[um]
     # axis4_resolution: Axis 4の分解能[um]
     # 分解能は次の値のいずれかを指定することができます[um]: 2, 1, 0.5, 0.4, 0.25, 0.2, 0.1, 0.08, 0.05, 0.04, 0.025, 0.02, 0.016, 0.01, 0.008
+    # 分解能を細かくすればするほど1パルス当たりの移動量が小さくなります。そのため、細かく動かすことができますが、その代わり移動速度が遅くなります。
     # クローズドループ制御を行う際はステージのセンサー分解能以上に設定してください
     def setResolution(self, axis1_resolution=1, axis2_resolution=1, axis3_resolution=1, axis4_resolution=1):
         valid_resolutions_um = [2, 1, 0.5, 0.4, 0.25, 0.2, 0.1, 0.08, 0.05, 0.04, 0.025, 0.02, 0.016, 0.01, 0.008]
@@ -83,10 +93,18 @@ class StageController:
         if axis1_resolution not in valid_resolutions_um or axis2_resolution not in valid_resolutions_um or axis3_resolution not in valid_resolutions_um or axis4_resolution not in valid_resolutions_um:
             print("無効な分解能が指定されました。許可されている分解能は次の通りです[um]:", valid_resolutions_um)
             return
-        self.stage.write(f"S:1{axis1_resolution}")
-        self.stage.write(f"S:2{axis2_resolution}")
-        self.stage.write(f"S:3{axis3_resolution}")
-        self.stage.write(f"S:4{axis4_resolution}")
+        self.axis1_resolution_um = axis1_resolution
+        self.axis2_resolution_um = axis2_resolution
+        self.axis3_resolution_um = axis3_resolution
+        self.axis4_resolution_um = axis4_resolution
+        self.stage.write(f"S:1{int(2/axis1_resolution)}")
+        self.stage.write(f"S:2{int(2/axis2_resolution)}")
+        self.stage.write(f"S:3{int(2/axis3_resolution)}")
+        self.stage.write(f"S:4{int(2/axis4_resolution)}")
+        print(f"S:1{int(2/axis1_resolution)}")
+        print(f"S:2{int(2/axis2_resolution)}")
+        print(f"S:3{int(2/axis3_resolution)}")
+        print(f"S:4{int(2/axis4_resolution)}")
         print(f"Resolution set to Axis 1: {axis1_resolution}, Axis 2: {axis2_resolution}, Axis 3: {axis3_resolution}, Axis 4: {axis4_resolution}")
         self.waitReady()
     
@@ -95,13 +113,13 @@ class StageController:
         if not self.check_connection():
             return
         if axis == 1:
-            num_pulse = int(position_um / self.axis1_resolution_um)
+            num_pulse = int(position_um * 20 / (BASE_LATE1 * self.axis1_resolution_um))
         elif axis == 2:
-            num_pulse = int(position_um / self.axis2_resolution_um)
+            num_pulse = int(position_um * 20 / (BASE_LATE2 * self.axis2_resolution_um))
         elif axis == 3:
-            num_pulse = int(position_um / self.axis3_resolution_um)
+            num_pulse = int(position_um * 20 / (BASE_LATE3 * self.axis3_resolution_um))
         elif axis == 4:
-            num_pulse = int(position_um / self.axis4_resolution_um)
+            num_pulse = int(position_um * 20 / (BASE_LATE4 * self.axis4_resolution_um))
         else:
             raise ValueError(f"無効な軸番号が指定されました: {axis}")
         
@@ -115,17 +133,17 @@ class StageController:
             time.sleep(wait_time_s)
     
     # 相対位置移動
-    def moveRel(self, axis, position_um, direction='+', wait_time_s=0.0):
+    def moveRel(self, axis, distance_um, direction='+', wait_time_s=0.0):
         if not self.check_connection():
             return
         if axis == 1:
-            num_pulse = int(position_um / self.axis1_resolution_um)
+            num_pulse = int(distance_um * 20 / (BASE_LATE1 * self.axis1_resolution_um))
         elif axis == 2:
-            num_pulse = int(position_um / self.axis2_resolution_um)
+            num_pulse = int(distance_um * 20 / (BASE_LATE2 * self.axis2_resolution_um))
         elif axis == 3:
-            num_pulse = int(position_um / self.axis3_resolution_um)
+            num_pulse = int(distance_um * 20 / (BASE_LATE3 * self.axis3_resolution_um))
         elif axis == 4:
-            num_pulse = int(position_um / self.axis4_resolution_um)
+            num_pulse = int(distance_um * 20 / (BASE_LATE4 * self.axis4_resolution_um))
         else:
             raise ValueError(f"無効な軸番号が指定されました: {axis}")
         
@@ -162,6 +180,12 @@ if __name__ == '__main__':
     # 原点に戻る
     stage_controller.moveToHomePosition(1)
     time.sleep(1)
+    
+    # 速度を設定
+    stage_controller.setSpeed(2000, 3000, 100, 2000, 3000, 100, 2000, 3000, 100, 2000, 3000, 100)
+    
+    # 分解能を設定
+    stage_controller.setResolution(1, 1, 1, 1)
     
     # 10000 um 移動
     stage_controller.moveAbs(1, 10000)
